8addedc0150434116dfabef6bdc1f939cfceb676
Leo UMAN <luman@e3r13p1.42.fr>
2018-02-22 09:03:12 +0100

    corrections norme

--- Training/level1/rev_print.c
+++ Training/level1/rev_print.c
@@ -6,7 +6,7 @@
 /*   By: luman <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 08:53:16 by luman             #+#    #+#             */
-/*   Updated: 2018/02/22 08:57:19 by luman            ###   ########.fr       */
+/*   Updated: 2018/02/22 09:02:56 by luman            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	int i;
+
 	i = 0;
 	if (argc == 2)
 	{
